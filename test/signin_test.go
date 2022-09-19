f80c5e185e7ed86fcd83ed295440c5d08a383d25
Murilo Correia <murilo.correiab@gmail.com>
2022-09-19 17:41:57 -0300

    test(signIn): Create more scenarios on integration tests

--- test/signin_test.go
+++ test/signin_test.go
@@ -13,20 +13,45 @@ import (
 func TestSignIn(t *testing.T) {
 	tests := []testRunner{
 		{"SignInSuccess", testSignInSuccess},
+		{"TryToSignInUserDontExists", testTryToSignInUserDontExists},
+		{"TryToSignInUserWrongCredentials", testTryToSignInUserWrongCredentials},
 	}
 	newSuite(t, tests).run()
 }
 
 func testSignInSuccess(t *testing.T) {
-	usecase := usecase.NewUserUseCase(adapter.NewUserRepository(databaseConnection))
+	usecase := createUseCase()
 	userPayload := framework.UserPayload{Username: "username", Password: "password"}
 
 	createUserOnDatabase(usecase, userPayload, t)
 
-	newApiTest(t, usecase).
-		setRequest(http.MethodPost, "/signin", userPayload).
-		execute().
-		checkStatusCode(201)
+	executeCallToApi(t, usecase, userPayload).checkStatusCode(201)
+}
+
+func testTryToSignInUserDontExists(t *testing.T) {
+	usecase := createUseCase()
+	userOnDatabase := framework.UserPayload{Username: "username", Password: "password"}
+
+	createUserOnDatabase(usecase, userOnDatabase, t)
+
+	userOnSignin := framework.UserPayload{Username: "username2", Password: "password"}
+
+	executeCallToApi(t, usecase, userOnSignin).checkStatusCode(403)
+}
+
+func testTryToSignInUserWrongCredentials(t *testing.T) {
+	usecase := createUseCase()
+	userOnDatabase := framework.UserPayload{Username: "username", Password: "password"}
+
+	createUserOnDatabase(usecase, userOnDatabase, t)
+
+	userOnSignin := framework.UserPayload{Username: userOnDatabase.Username, Password: "password2"}
+
+	executeCallToApi(t, usecase, userOnSignin).checkStatusCode(403)
+}
+
+func createUseCase() usecase.UserUseCase {
+	return usecase.NewUserUseCase(adapter.NewUserRepository(databaseConnection))
 }
 
 func createUserOnDatabase(uc usecase.UserUseCase, userPayload framework.UserPayload, t *testing.T) {
@@ -35,4 +60,10 @@ func createUserOnDatabase(uc usecase.UserUseCase, userPayload framework.UserPayl
 	if err != nil {
 		t.Fatal("Error creating user on database", err)
 	}
+}
+
+func executeCallToApi(t *testing.T, usecase usecase.UserUseCase, payload framework.UserPayload) *apiTest {
+	return newApiTest(t, usecase).
+		setRequest(http.MethodPost, "/signin", payload).
+		execute()
 }
